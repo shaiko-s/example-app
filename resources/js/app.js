9a4cc43d04e25fa515330358daaf282ff1fcaa7b
shaiko-s <sergii.shaiko@gmail.com>
2024-09-28 13:13:41 +0200

    Refactor app.js to dynamically set layout for pages

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,26 +2,30 @@ import '../css/app.css';
 import './bootstrap';
 
 import { createInertiaApp } from '@inertiajs/vue3';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createApp, h } from 'vue';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import Layout from './Layouts/GuestLayout.vue';
+
+import { Head } from '@inertiajs/vue3';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) =>
-        resolvePageComponent(
-            `./Pages/${name}.vue`,
-            import.meta.glob('./Pages/**/*.vue'),
-        ),
+    resolve: name => {
+      const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
+      let page = pages[`./Pages/${name}.vue`]
+      page.default.layout = name.startsWith('Public/') ? undefined : (page.default.layout || Layout)
+      return page
+    },
     setup({ el, App, props, plugin }) {
         return createApp({ render: () => h(App, props) })
             .use(plugin)
+            .component('Head', Head)
             .use(ZiggyVue)
             .mount(el);
     },
     progress: {
         color: '#4B5563',
     },
-});
+})
