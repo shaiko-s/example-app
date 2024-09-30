1ea46d3fe69f880fe3ae211eea7479657dc9e753
shaiko-s <sergii.shaiko@gmail.com>
2024-09-30 21:01:26 +0200

    Add routes for ingredients, semiproducts, and products

--- routes/web.php
+++ routes/web.php
@@ -24,4 +24,16 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/ingredients', function () {
+    return 'Hi there Ingredients';
+})->name('ingredients');
+
+Route::get('/semiproducts', function () {
+    return 'Hi there Semiproducts';
+})->name('semiproducts');
+
+Route::get('/products', function () {
+    return 'Hi there Products';
+})->name('products');
+
 require __DIR__.'/auth.php';
