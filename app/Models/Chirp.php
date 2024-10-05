3f7fdd74a1ef2154d7a9d16c2a5e15f79b714280
shaiko-s <sergii.shaiko@gmail.com>
2024-10-05 20:10:18 +0300

    Refactor Chirp model to add fillable property and chirps relationship

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -4,8 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Chirp extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'message',
+    ];
+
+    public function chirps(): HasMany
+    {
+        return $this->hasMany(Chirp::class);
+    }
 }
