a4c7bb75538b9c25969c6bfbb1c4ae0be8041e7a
shaiko-s <sergii.shaiko@gmail.com>
2024-10-06 17:54:35 +0300

    Refactor IngredientController to include authorization and update functionality

--- app/Http/Controllers/IngredientController.php
+++ app/Http/Controllers/IngredientController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Ingredient;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
 use Inertia\Inertia;
 use Inertia\Response;
 
@@ -61,9 +62,17 @@ class IngredientController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Ingredient $ingredient)
+    public function update(Request $request, Ingredient $ingredient): RedirectResponse
     {
-        //
+        Gate::authorize('update', $ingredient);
+
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+        ]);
+
+        $ingredient->update($validated);
+
+        return redirect(route('ingredient.index'));
     }
 
     /**
