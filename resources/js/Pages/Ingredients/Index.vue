8a91de9a1ab11933e04d2866e129e149cb0fd617
shaiko-s <sergii.shaiko@gmail.com>
2024-10-06 15:06:44 +0300

    Refactor ingredient form in Index.vue to use 'name' instead of 'message'

--- resources/js/Pages/Ingredients/Index.vue
+++ resources/js/Pages/Ingredients/Index.vue
@@ -5,7 +5,7 @@ import PrimaryButton from '@/Components/PrimaryButton.vue';
 import { useForm, Head } from '@inertiajs/vue3';
 
 const form = useForm({
-    message: '',
+    name: '',
 });
 </script>
 
@@ -21,10 +21,10 @@ export default {
 
     <div class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
         <form @submit.prevent="form.post(route('ingredients.store'), { onSuccess: () => form.reset() })">
-            <textarea v-model="form.message"
+            <textarea v-model="form.name"
                       placeholder="Name of the ingredient"
                       class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"></textarea>
-            <InputError :message="form.errors.message"
+            <InputError :name="form.errors.name"
                         class="mt-2" />
             <PrimaryButton class="mt-4">Create</PrimaryButton>
         </form>
