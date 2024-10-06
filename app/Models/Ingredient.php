1261965edb13fe8d3a052a567b619254260cbaf9
shaiko-s <sergii.shaiko@gmail.com>
2024-10-06 15:20:55 +0300

    Refactor Ingredient model to include user relationship

--- app/Models/Ingredient.php
+++ app/Models/Ingredient.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Ingredient extends Model
 {
@@ -12,4 +13,9 @@ class Ingredient extends Model
     protected $fillable = [
         'name',
     ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
