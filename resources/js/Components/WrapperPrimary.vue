3c471288eed429f2f131f582c2442a2bdd6ff8b8
shaiko-s <sergii.shaiko@gmail.com>
2024-10-07 16:23:48 +0300

    Refactor WrapperPrimary.vue component for page content wrapper

---
+++ resources/js/Components/WrapperPrimary.vue
@@ -0,0 +1,10 @@
+<template>
+    <!-- Page content wrapper -->
+    <div class="py-12">
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800">
+                <slot />
+            </div>
+        </div>
+    </div>
+</template>
