16bd4e1b80aa0e4a046c1fab679c39e99debf231
shaiko-s <sergii.shaiko@gmail.com>
2024-10-07 22:53:54 +0300

    Refactor Pagination.vue and PrimaryButton.vue components for consistent styling

--- resources/js/Components/Pagination.vue
+++ resources/js/Components/Pagination.vue
@@ -12,7 +12,11 @@ const props = defineProps({
     <!-- Pagination -->
     <div class="flex items-center justify-between mt-6 mb-6">
         <Link :href="pagination.prev_page_url || '#'"
-              class="flex items-center px-5 py-2 text-sm text-gray-700 capitalize transition-colors duration-200 bg-white border rounded-md gap-x-2 hover:bg-gray-100 dark:bg-gray-900 dark:text-gray-200 dark:border-gray-700 dark:hover:bg-gray-800">
+
+              class="flex items-center px-5 py-2 text-sm  capitalize transition-colors duration-200  border rounded-md gap-x-2"
+              :class="!pagination.prev_page_url
+                    ? 'text-gray-500 bg-white dark:bg-gray-600 dark:text-gray-500 dark:border-gray-700 cursor-default'
+                    : 'text-gray-500 bg-white dark:bg-gray-900 dark:hover:bg-gray-700 dark:text-gray-300 dark:hover:text-white hover:bg-gray-100'">
         <svg xmlns="http://www.w3.org/2000/svg"
              fill="none"
              viewBox="0 0 24 24"
@@ -35,14 +39,17 @@ const props = defineProps({
                   :href="link.url"
                   class="px-2 py-1 text-sm rounded-md"
                   :class="link.active
-                    ? 'text-blue-500 dark:bg-gray-900 bg-blue-100/60'
-                    : 'text-gray-500 dark:hover:bg-gray-800 dark:text-gray-300 hover:bg-gray-100'">
+                    ? 'text-blue-500 dark:bg-gray-900 bg-blue-100/60 cursor-default'
+                    : 'text-gray-500 dark:hover:bg-gray-700 dark:text-gray-300 hover:bg-gray-100'">
             {{ link.label }}
             </Link>
         </div>
 
         <Link :href="pagination.next_page_url || '#'"
-              class="flex items-center px-5 py-2 text-sm text-gray-700 capitalize transition-colors duration-200 bg-white border rounded-md gap-x-2 hover:bg-gray-100 dark:bg-gray-900 dark:text-gray-200 dark:border-gray-700 dark:hover:bg-gray-800">
+        class="flex items-center px-5 py-2 text-sm  capitalize transition-colors duration-200  border rounded-md gap-x-2"
+              :class="!pagination.next_page_url
+                    ? 'text-gray-500 bg-white dark:bg-gray-600 dark:text-gray-500 dark:border-gray-700 cursor-default'
+                    : 'text-gray-500 bg-white dark:bg-gray-900 dark:hover:bg-gray-700 dark:text-gray-300 dark:hover:text-white hover:bg-gray-100'">
         <span>
             Next
         </span>
