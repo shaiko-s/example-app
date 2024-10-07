8f700d99ee94a8dbf8097bd333e3d172d59a9ea0
shaiko-s <sergii.shaiko@gmail.com>
2024-10-07 16:25:01 +0300

    Refactor app.js to import and use Vue 3 composition API

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,13 +2,21 @@ import '../css/app.css';
 import './bootstrap';
 
 import { createInertiaApp } from '@inertiajs/vue3';
-import { createApp, h } from 'vue';
+import { createApp, h, toRaw } from 'vue';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 import Layout from './Layouts/GuestLayout.vue';
 
 import { Head } from '@inertiajs/vue3';
+import { computed, ref } from 'vue';
+import { usePage } from '@inertiajs/vue3';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+
+const page = ref(usePage());
+console.log('page', page);
+
+const value = ref(page.value);
+console.log('page.value', value);
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
