aae6769d99f720227b1f757ac3df702ee7a1bb66
shaiko-s <sergii.shaiko@gmail.com>
2024-10-07 16:24:03 +0300

    Refactor SectionHeader.vue component for displaying section title and total count

---
+++ resources/js/Components/SectionHeader.vue
@@ -0,0 +1,23 @@
+<script setup>
+defineProps({
+    title: String,
+    total: Number,
+});
+
+</script>
+
+<template>
+    <!-- Table head -->
+    <div class="flex items-center gap-x-3">
+
+        <!-- Title -->
+        <h2 class="text-lg font-medium text-gray-800 dark:text-white">
+            {{ title }}
+        </h2>
+
+        <!-- Total ingredients -->
+        <span class="px-3 py-1 text-xs text-blue-600 bg-blue-100 rounded-full dark:bg-gray-900 dark:text-blue-400">
+            {{ `${total} ${title.toLowerCase()}` }}
+        </span>
+    </div>
+</template>
