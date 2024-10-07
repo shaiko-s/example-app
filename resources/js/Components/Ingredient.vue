e428f7c444ef8fb8c9b3a25c8318bb5385c2b488
shaiko-s <sergii.shaiko@gmail.com>
2024-10-07 16:24:20 +0300

    Refactor Ingredient.vue component for editing ingredient name and displaying user and creation details

--- resources/js/Components/Ingredient.vue
+++ resources/js/Components/Ingredient.vue
@@ -1,10 +1,12 @@
 <script setup>
-import InputError from '@/Components/InputError.vue';
-import PrimaryButton from '@/Components/PrimaryButton.vue';
+import FormEditOneField from './FormEditOneField.vue';
 import dayjs from 'dayjs';
 import { useForm, Link } from '@inertiajs/vue3';
 import { ref } from 'vue';
-import TextInput from './TextInput.vue';
+import Checkbox from './Checkbox.vue';
+import TableData from './TableData.vue';
+import IconEdit from './IconEdit.vue';
+import IconDelete from './IconDelete.vue';
 
 const props = defineProps(['ingredient']);
 
@@ -17,70 +19,63 @@ const editing = ref(false);
 
 <template>
     <tr>
-        <td class="px-4 py-4 text-sm font-medium text-gray-700 whitespace-nowrap">
-            <form v-if="editing" @submit.prevent="form.put(route('ingredients.update', ingredient.id), { onSuccess: () => editing = false })">
-                <TextInput v-model="form.name" class="mt-4 w-full text-gray-900 border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"></TextInput>
-                <InputError :message="form.errors.name" class="mt-2" />
-                <div class="space-x-2">
-                    <PrimaryButton class="mt-4">Save</PrimaryButton>
-                    <button class="mt-4" @click="editing = false; form.reset(); form.clearErrors()">Cancel</button>
-                </div>
-            </form>
-            <div v-else class="inline-flex items-center gap-x-3">
-                <input type="checkbox"
-                       class="text-blue-500 border-gray-300 rounded dark:bg-gray-900 dark:ring-offset-gray-900 dark:border-gray-700"
-                       :id="ingredient.id">
+        <!-- ingredient name / Form Editing -->
+        <TableData>
+
+            <!-- Form Editing -->
+            <div v-if="editing">
+                <FormEditOneField :ingredient="ingredient"
+                                  :editing="editing"
+                                  @update:editing="editing = false"
+                                  @handleCancel="editing = false" />
+            </div>
+
+            <!-- Ingredient name -->
+            <div v-else
+                 class="inline-flex items-center gap-x-3">
+                <Checkbox :checked="ingredient.checked || false"
+                          :value="ingredient.id"
+                          @update:checked="ingredient.checked = $event" />
                 <h2 class="font-medium text-gray-800 dark:text-white ml-2">
                     {{ ingredient.name }}</h2>
             </div>
-        </td>
-        <td class="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">
-            {{ ingredient.user.name }}</td>
-        <td class="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">
+        </TableData>
+
+        <!-- user name -->
+        <TableData>
+            {{ ingredient.user.name }}
+        </TableData>
+
+        <!-- created at -->
+        <TableData>
             {{ dayjs(ingredient.created_at).format('DD-MM-YYYY HH:mm') }}
-            <small v-if="ingredient.created_at !== ingredient.updated_at" class="text-sm text-gray-600"> &middot; edited</small>
-        </td>
+            <small v-if="ingredient.created_at !== ingredient.updated_at"
+                   class="text-sm text-gray-600"> &middot; edited</small>
+        </TableData>
+
         <!-- edit - delete buttons -->
-        <td class="px-4 py-4 text-sm whitespace-nowrap">
+        <TableData>
             <div v-if="ingredient.user.id === $page.props.auth.user.id"
-             class="flex items-center gap-x-6">
-                                <!-- edit button-->
-                                <button
-                        class="text-gray-500 transition-colors duration-200 dark:hover:text-yellow-500 dark:text-gray-300 hover:text-yellow-500 focus:outline-none"
-                        @click="editing = true">
-                    <svg xmlns="http://www.w3.org/2000/svg"
-                         fill="none"
-                         viewBox="0 0 24 24"
-                         stroke-width="1.5"
-                         stroke="currentColor"
-                         class="w-5 h-5 text-gray-300">
-                        <path stroke-linecap="round"
-                              stroke-linejoin="round"
-                              d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
-                    </svg>
+                 class="flex items-center gap-x-6">
+
+                <!-- edit button-->
+                <button class="text-gray-500 transition-colors duration-200
+                            dark:hover:text-yellow-500 dark:text-gray-300 hover:text-yellow-500 focus:outline-none"
+                        @click="editing = !editing">
+                    <IconEdit />
                 </button>
 
                 <!-- delete button-->
-                <Link :href="route('ingredients.destroy', ingredient.id)" as="button" method="delete">
-                    <button
-                            class="text-gray-500 transition-colors duration-200 dark:hover:text-red-500 dark:text-gray-300 hover:text-red-500 focus:outline-none"
-                            >
-                        <svg xmlns="http://www.w3.org/2000/svg"
-                             fill="none"
-                             viewBox="0 0 24 24"
-                             stroke-width="1.5"
-                             stroke="currentColor"
-                             class="w-5 h-5 text-gray-500">
-                            <path stroke-linecap="round"
-                                  stroke-linejoin="round"
-                                  d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
-                        </svg>
-                    </button>
+                <Link :href="route('ingredients.destroy', ingredient.id)"
+                      as="button"
+                      method="delete">
+                <button
+                        class="text-gray-500 transition-colors duration-200 dark:hover:text-red-500 dark:text-gray-300 hover:text-red-500 focus:outline-none">
+                    <IconDelete />
+                </button>
                 </Link>
-
-
             </div>
-        </td>
+        </TableData>
     </tr>
 
 </template>
